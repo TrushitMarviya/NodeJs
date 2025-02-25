4fb1f6665502c5a0c4cab386f7135e2528a8da89
Trushit Marviya <marviyatrushit0@gmail.com>
2025-02-25 14:54:50 +0530

    Admin-panel-2

--- Admin-Panel-2 - Passport/Config/db.js
+++ Admin-Panel-2 - Passport/Config/db.js
@@ -1,8 +1,9 @@
-const { log } = require("console");
 const mongoose = require("mongoose");
-mongoose.connect("mongodb://127.0.0.1/Admin_2_Passport");
+mongoose.connect(
+  "mongodb+srv://marviyatrushit0:Trushit_001@cluster0.2q5ww.mongodb.net/?retryWrites=true&w=majority&appName=Cluster0"
+);
 const db = mongoose.connection;
-db.once("open",(err)=>{
-    err?console.log(err):console.log("MongoDb Connected");
-})
+db.once("open", (err) => {
+  err ? console.log(err) : console.log("MongoDb Connected");
+});
 module.exports = mongoose;
--- Admin-Panel-2 - Passport/index.js
+++ Admin-Panel-2 - Passport/index.js
@@ -7,7 +7,6 @@ const cookie = require("cookie-parser");
 const passport = require("./Middleware/Passport");
 const session = require("express-session");
 
-
 app.set("view engine","ejs");
 app.use(express.urlencoded({extended:true}));
 app.use(express.static(path.join(__dirname,"public")));
